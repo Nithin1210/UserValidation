2b5a9ffddeb83b045970ce7c21bf41d886e1790e
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-08-02 22:49:40 +0530

    [Nithin] Add : Updation of FirstName

---
+++ UserValidation/Program.cs
@@ -0,0 +1,13 @@
+﻿using UserValidation;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("UC1 First_Name :");
+        ValidationUser firstname = new ValidationUser();
+        firstname.uc1Validation("Nithin");
+
+    }
+
+}
---
+++ UserValidation/ValidationUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserValidation
+{
+    public class ValidationUser
+    {
+        string uc1firstname = "^[A-Z]{1}[a-z]{2,}$";
+
+        public void uc1Validation(string input)
+        {
+            bool result = Regex.IsMatch(input, uc1firstname);
+            if (result)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("! inValid");
+        }
+  
+
+    }
+
+}
