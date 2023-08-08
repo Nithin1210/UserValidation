3467011a3e1c6c27517102c09d644c93dd91c5b7
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-08-08 14:54:38 +0530

    [Nithin] Add : Updation of All emails check

--- UserValidation/Program.cs
+++ UserValidation/Program.cs
@@ -1,28 +1,38 @@
 ﻿using UserValidation;
 
-internal class Program
+public class Program
 {
-    private static void Main(string[] args)
+    static string[] allemails = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" };
+
+    public static void Main(string[] args)
     {
         Console.WriteLine("UC1 First_Name :");
         ValidationUser firstname = new ValidationUser();
         firstname.uc1Validation("Nithin");
 
-        Console.WriteLine("UC2 Last_Name :");
+        Console.WriteLine("\nUC2 Last_Name :");
         ValidationUser lastname = new ValidationUser();
         lastname.uc2Validation("Reddy");
 
-        Console.WriteLine("UC3 Email");
+        Console.WriteLine("\nUC3 Email");
         ValidationUser email = new ValidationUser();
         email.uc3Validation("nithin@reddy.com");
 
-        Console.WriteLine("UC4 PhoneNumber");
+        Console.WriteLine("\nUC4 PhoneNumber");
         ValidationUser num = new ValidationUser();
         num.uc4Validation("91 1234567890");
 
-        Console.WriteLine("UC5 Password");
+        Console.WriteLine("\nUC5 Password");
         ValidationUser pass = new ValidationUser();
         pass.uc5Validation("Qwertyuiop@123");
+
+        Console.WriteLine("\nAll Emails check !! \nSample Emails to Check !!");
+        ValidationUser Email = new ValidationUser();
+        foreach (string emails in allemails)
+        {
+            Console.WriteLine(emails);
+            email.uc3Validation(emails);
+        }
 
     }
 
